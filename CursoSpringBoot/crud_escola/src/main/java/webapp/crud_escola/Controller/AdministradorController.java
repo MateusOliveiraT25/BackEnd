954121d4beeecc59c04880d74e104d0a6ad67531
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2024-03-28 21:56:11 -0300

    28/03/24

--- CursoSpringBoot/crud_escola/src/main/java/webapp/crud_escola/Controller/AdministradorController.java
+++ CursoSpringBoot/crud_escola/src/main/java/webapp/crud_escola/Controller/AdministradorController.java
@@ -2,32 +2,82 @@ package webapp.crud_escola.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import webapp.crud_escola.Model.Administrador;
 import webapp.crud_escola.Repository.AdministradorRepository;
 import webapp.crud_escola.Repository.VerificaCadastroAdmRepository;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-public class AdministradorController {
+public class AdministradorController  {
     @Autowired
     private AdministradorRepository ar ;
-
     @Autowired
     private VerificaCadastroAdmRepository vcar;
 
     @PostMapping("/cad-adm")
-    public String postCadAdm (Administrador adm) {
-        String cpfVerificar = vcar.findByCpf(adm.getCpf()).getCpf();
-        if (cpfVerificar == adm.getCpf()) {
-            ar.save(adm);  
+    public ModelAndView postCadAdm (Administrador adm) {
+        boolean verificaCpf = vcar.existsById(adm.getCpf());
+
+        ModelAndView mv = new ModelAndView("login-adm");
+        if (verificaCpf) {
+            ar.save(adm);
+            String mensagem = "Cadastro Realizado com sucesso";
+            System.out.println(mensagem);
+            mv.addObject("msg", mensagem);
+        } else {
+            String mensagem = "Cadastro não realizado . . .";
+            System.out.println(mensagem);
+            mv.addObject("msg", mensagem);
         }
-       
-        return "/login-adm";
+      
+        return mv;
+    }
+
+    @PostMapping("/acesso-adm")
+public ModelAndView acessoAdmLogin(@RequestParam(required = false) String cpf,
+                                    @RequestParam(required = false) String senha) {
+    ModelAndView mv = new ModelAndView();
+
+    if (cpf == null || cpf.isEmpty()) {
+        String mensagem = "CPF não informado";
+        System.out.println(mensagem);
+        mv.addObject("msg", mensagem);
+        mv.setViewName("login-adm");
+        return mv;
+    }
+
+    if (senha == null || senha.isEmpty()) {
+        String mensagem = "Senha não informada";
+        System.out.println(mensagem);
+        mv.addObject("msg", mensagem);
+        mv.setViewName("login-adm");
+        return mv;
+    }
+
+    // Verifica se o CPF e a senha correspondem a algum registro de administrador
+    boolean acessoCPF = cpf.equals(ar.findByCpf(cpf).getCpf());
+    boolean acessoSenha = senha.equals(ar.findByCpf(cpf).getSenha());
+    if (acessoSenha && acessoCPF) {
+        String mensagem = "Login Realizado com sucesso";
+        System.out.println(mensagem);
+        mv.addObject("msg", mensagem);
+        mv.setViewName("sucesso");
+    } else {
+        String mensagem = "Login não efetuado";
+        System.out.println(mensagem);
+        mv.addObject("msg", mensagem);
+        mv.setViewName("login-adm");
     }
     
-    
+    return mv;
 }
+
+}
+
+    
+    
--- CursoSpringBoot/crud_escola/src/main/java/webapp/crud_escola/Repository/AdministradorRepository.java
+++ CursoSpringBoot/crud_escola/src/main/java/webapp/crud_escola/Repository/AdministradorRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import webapp.crud_escola.Model.Administrador;
 
+
+
 public interface AdministradorRepository extends CrudRepository <Administrador, String>{
+    Administrador  findByCpf(String cpf);
+    Administrador  findBySenha(String senha);
     
 }
