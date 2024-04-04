911f68da30977a36d95996f3c54c0710daf78ed9
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-04-04 09:42:55 -0300

    04/04/24

--- CursoSpringBoot/crud_escola/src/main/java/webapp/crud_escola/Controller/AdministradorController.java
+++ CursoSpringBoot/crud_escola/src/main/java/webapp/crud_escola/Controller/AdministradorController.java
@@ -17,81 +17,92 @@ import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-public class AdministradorController  {
+public class AdministradorController {
     @Autowired
     private AdministradorRepository ar ;
     @Autowired
     private VerificaCadastroAdmRepository vcar;
 
 
-@PostMapping("cad-adm")
-public ModelAndView postCadAdm(Administrador adm) {
-    ModelAndView mv = new ModelAndView("login-adm");
-    boolean verificaCpf = vcar.existsById(adm.getCpf());
+    @PostMapping("cad-adm")
+    public ModelAndView postCadAdm(Administrador adm) {
+        ModelAndView mv = new ModelAndView("login-adm");
+        boolean verificaCpf = vcar.existsById(adm.getCpf());
 
-    if (!verificaCpf) { // Se o CPF não existe, procede com o cadastro
-        ar.save(adm);
-        mv.addObject("msg", "Cadastro Realizado com sucesso");
-    } else {
-        mv.addObject("msg", "Cadastro não realizado. CPF já cadastrado.");
-    }
-    return mv;
-}
-@PostMapping("acesso-adm")
-public ModelAndView acessoAdmLogin(@RequestParam String cpf, @RequestParam String senha, HttpServletRequest request) {
-    ModelAndView mv = new ModelAndView();
-    Administrador adm = ar.findByCpf(cpf);
-
-    if (adm != null) {
-        boolean acessoCPF = cpf.equals(adm.getCpf());
-        boolean acessoSenha = senha.equals(adm.getSenha());
-
-        if (acessoCPF && acessoSenha) {
-            String mensagem = "Login Realizado com sucesso";
-            System.out.println(mensagem);
-
-            // Adicionar usuário à sessão
-            HttpSession session = request.getSession();
-            session.setAttribute("usuario_nome", adm.getNome());
-            session.setAttribute("usuario_cpf", adm.getCpf());
-
-            mv.setViewName("redirect:/interna-adm");
+        if (!verificaCpf) { // Se o CPF não existe, procede com o cadastro
+            ar.save(adm);
+            mv.addObject("msg", "Cadastro Realizado com sucesso");
         } else {
-            String mensagem = "Login Não Efetuado";
+            mv.addObject("msg", "Cadastro não realizado. CPF já cadastrado.");
+        }
+        return mv;
+    }
+
+    @PostMapping("acesso-adm")
+    public ModelAndView acessoAdmLogin(@RequestParam String cpf, @RequestParam String senha, HttpServletRequest request) {
+        ModelAndView mv = new ModelAndView();
+        Administrador adm = ar.findByCpf(cpf);
+
+        if (adm != null) {
+            boolean acessoCPF = cpf.equals(adm.getCpf());
+            boolean acessoSenha = senha.equals(adm.getSenha());
+
+            if (acessoCPF && acessoSenha) {
+                String mensagem = "Login Realizado com sucesso";
+                System.out.println(mensagem);
+
+                // Adicionar usuário à sessão
+                HttpSession session = request.getSession();
+                session.setAttribute("usuario_id", adm.getCpf());
+                session.setAttribute("usuario_nome", adm.getNome());
+
+                mv.setViewName("redirect:/interna-adm");
+            } else {
+                String mensagem = "Login Não Efetuado";
+                System.out.println(mensagem);
+                mv.addObject("msg", mensagem);
+                mv.addObject("classe", "vermelho");
+                mv.setViewName("login-adm");
+            }
+        } else {
+            // Tratamento para CPF não cadastrado
+            String mensagem = "CPF não cadastrado ou dados incorretos";
             System.out.println(mensagem);
             mv.addObject("msg", mensagem);
             mv.addObject("classe", "vermelho");
             mv.setViewName("login-adm");
         }
-    } else {
-        // Tratamento para CPF não cadastrado
-        String mensagem = "CPF não cadastrado ou dados incorretos";
-        System.out.println(mensagem);
-        mv.addObject("msg", mensagem);
-        mv.addObject("classe", "vermelho");
-        mv.setViewName("login-adm");
+
+        return mv;
     }
 
-    return mv;
-}
+    @GetMapping("interna-adm")
+    public String acessoPageInternaAdm(HttpServletRequest request) {
+        ModelAndView mv = new ModelAndView();
+        HttpSession session = request.getSession();
+        boolean acessoInternoAdm = session.getAttribute("usuario_id") != null;
 
-@GetMapping("interna-adm")
-public String acessoPageInternaAdm(HttpServletRequest request) {
-    ModelAndView mv = new ModelAndView();
-    HttpSession session = request.getSession();
-    boolean acessoInternoAdm = session.getAttribute("usuario_cpf") != null;
+        String acesso = "";
+        if (acessoInternoAdm) {
+            acesso = "interna-adm";
+        } else {
+            acesso = "login-adm";
+            String mensagem = "Acesso não Permitido - faça Login";
+            System.out.println(mensagem);
+            mv.addObject("msg", mensagem);
+            mv.addObject("classe", "vermelho");
+        }
 
-    String acesso = "";
-    if (acessoInternoAdm) {
-        acesso = "interna-adm";
-    } else {
-        acesso = "login-adm";
-        String mensagem = "Acesso não Permitido - faça Login";
-        System.out.println(mensagem);
-        mv.addObject("msg", mensagem);
-        mv.addObject("classe", "vermelho");
+        return acesso;
     }
 
-    return acesso;
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate(); // Invalida a sessão atual
+        }
+        return "redirect:/login-adm"; // Redireciona para a página de login
+    }
 }
-}
+
