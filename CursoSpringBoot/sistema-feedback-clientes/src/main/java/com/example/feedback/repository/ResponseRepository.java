c0585c005fc164b7e8182757f344c6fad2d2c75f
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-07-30 10:36:44 -0300

    30/07/24

--- CursoSpringBoot/sistema-feedback-clientes/src/main/java/com/example/feedback/repository/ResponseRepository.java
+++ CursoSpringBoot/sistema-feedback-clientes/src/main/java/com/example/feedback/repository/ResponseRepository.java
@@ -3,5 +3,8 @@ package com.example.feedback.repository;
 import com.example.feedback.model.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResponseRepository extends JpaRepository<Response, Long> {
+    List<Response> findByFeedbackId(Long feedbackId);
 }
