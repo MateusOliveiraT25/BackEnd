61c1050fdbb5f04ade6e7226f2db0633c238e7f4
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-07-30 11:43:24 -0300

    30/07/24

--- CursoSpringBoot/sistema-feedback-clientes/src/main/java/com/example/feedback/model/Response.java
+++ CursoSpringBoot/sistema-feedback-clientes/src/main/java/com/example/feedback/model/Response.java
@@ -1,9 +1,15 @@
 package com.example.feedback.model;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Response {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,4 +21,28 @@ public class Response {
     private String responseText;
 
     // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Feedback getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(Feedback feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getResponseText() {
+        return responseText;
+    }
+
+    public void setResponseText(String responseText) {
+        this.responseText = responseText;
+    }
 }
